826d8e0e077041c6264b2551df2ef02c8a0bc25c
Victor Hugo <hey.zooi@gmail.com>
2023-09-09 10:09:46 -0400

    Stage 2 - Step 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 #[macro_use]
 extern crate rocket;
 
+#[macro_use]
+extern crate log;
+
+extern crate pretty_env_logger;
+
+use dotenvy::dotenv;
+
+use sqlx::postgres::PgPoolOptions;
+
 mod cors;
 mod handlers;
 mod models;
@@ -10,6 +19,25 @@ use handlers::*;
 
 #[launch]
 async fn rocket() -> _ {
+    pretty_env_logger::init();
+    dotenv().ok();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
+        .await
+        .expect("Failed to create Postgres connection pool!");
+    
+    // TODO: Delete this query
+    let recs = sqlx::query!("SELECT * FROM questions")
+            .fetch_all(&pool)
+            .await
+            .unwrap();
+
+    // TODO: Delete these log statements
+    info!("********* Question Records *********");
+    info!("{:?}", recs);
+
     rocket::build()
         .mount(
             "/",
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,4 @@
+use thiserror::Error;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -38,4 +39,17 @@ pub struct AnswerDetail {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AnswerId {
     pub answer_uuid: String
+}
+
+#[derive(Error, Debug)]
+pub enum DBError {
+    #[error("Invalid UUID provided: {0}")]
+    InvalidUUID(String),
+    #[error("Database error occurred")]
+    Other(#[from] Box<dyn std::error::Error + Send + Sync>),
+}
+
+// source: https://www.postgresql.org/docs/current/errcodes-appendix.html
+pub mod postgres_error_codes {
+    pub const FOREIGN_KEY_VIOLATION: &str = "23503";
 }
